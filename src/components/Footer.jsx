9ef53c6d4a64084abee6edd36889739a1db229d4
Victor Atoyeje <atoyejeovercomer2@gmail.com>
2026-04-04 00:29:19 +0100

    fix:Footer uI adjust

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import { generalPagePadding } from "../utils/constants";
 
 export function Footer() {
-  return (
-    <div className="flex flex-col mt-12 text-white ">
+  return ( 
+    <div className={` flex flex-col mt-12 text-white w-full `}>
       {/* <div className="w-full h-8 bg-primary "></div> */}
-      <div className="flex items-center justify-between py-12 px-16 bg-primary">
+      <div className={`${generalPagePadding} flex items-center justify-between py-12 px-16 bg-primary`}>
         <div className="flex items-center gap-4 [&_span]:opacity-60 [&_p]:opacity-60">
           <h1 className="font-bold text-2xl">Gioa</h1>
           <div className="flex flex-col border-l-2 border-neutral-600 pl-4 [&_span]:text-sm [&_p]:text-sm">
