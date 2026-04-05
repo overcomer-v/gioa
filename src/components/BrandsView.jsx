b48f2c86388da2e69e200f29014843102ffb5651
Victor Atoyeje <atoyejeovercomer2@gmail.com>
2026-04-05 21:07:21 +0100

    fix:Mobile UI adaptation

--- src/components/BrandsView.jsx
+++ src/components/BrandsView.jsx
@@ -3,6 +3,7 @@ import { useProducts } from "../hooks/databaseManager/useProducts";
 import { Spinner } from "./Spinners";
 import { Link } from "react-router-dom";
 import { generalPagePadding } from "../utils/constants";
+import { Subtitle } from "./Titles";
 
 export function BrandsView() {
   const { getBrandNames } = useProducts();
@@ -14,21 +15,30 @@ export function BrandsView() {
     });
   }, []);
 
-  return brands ? (
-    <div className={`flex flex-wrap gap-3 md:gap-4 md:py-6 pb-6 ${generalPagePadding} bg-white border-b-2 border-neutral-200 text-white max-60 overflow-y-scroll page-animation`}>
-      {brands.map((e) => (
-        <Link
-        to={`/group-opener/brand/${e}`}
-          key={e}
-          className="rounded-full md:py-2 md:px-4 py-1 px-3 border-2 border-neutral-200 text-primary hover:bg-primary hover:text-white md:text-sm text-xs"
+  return (
+    <div className={`md:mt-6 pt-3  ${generalPagePadding}  page-animation md:border-0 border-t-2 border-neutral-100`}>
+    <h2 className="mb-6 font-semibold text-2xl text-primary">
+      Brands
+    </h2>
+      {brands ? (
+        <div
+          className={`flex flex-wrap gap-3 md:gap-4 pb-6 bg-white border-b-2 border-neutral-200 text-white max-60 overflow-y-scroll no-scrollbar`}
         >
-          {e}
-        </Link>
-      ))}
-    </div>
-  ) : (
-    <div className=" flex w-full h-20 bg-white">
-      <Spinner size="text-xl opacity-60 m-auto text-neutral-500" />
+          {brands.map((e) => (
+            <Link
+              to={`/group-opener/brand/${e}`}
+              key={e}
+              className="rounded-full md:py-2 md:px-4 py-1 px-3 border-2 border-neutral-200 text-primary hover:bg-primary hover:text-white md:text-sm text-xs"
+            >
+              {e}
+            </Link>
+          ))}
+        </div>
+      ) : (
+        <div className=" flex w-full h-20 bg-white">
+          <Spinner size="text-xl opacity-60 m-auto text-neutral-500" />
+        </div>
+      )}
     </div>
   );
 }
